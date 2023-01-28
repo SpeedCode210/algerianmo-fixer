e88c4d973122272b2e5bd69fefa4b30de171eef4
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2023-01-28 10:09:30 +0000

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -5,7 +5,7 @@
 // @include     http://algerianmo.com/*
 // @downloadURL https://github.com/SpeedCode210/algerianmo-fixer/raw/main/script.user.js
 // @icon http://www.algerianmo.com/static/images/favicon.ico
-// @version     1.2.1
+// @version     1.2.2
 // @author      Raouf Ould Ali / SpeedCode#0050
 // @description 1/25/2023, 5:34:04 PM
 // ==/UserScript==
@@ -19,7 +19,7 @@ for (var i=0;i<links.snapshotLength;i++) {
   if(thisLink.textContent.includes("إنجازاتي"))
     thisLink.href = thisLink.href.replace(RegExp("(?<=algerianmo\.com\/accounts\/)([0-9]+)"),right_num);
 }
-if(window.location.href.match(/algerianmo.com\/accounts\/[0-9]/g).length >= 1){
+if(window.location.href.match(/algerianmo.com\/accounts\/[0-9]/g)){
 var zNode = document.getElementsByTagName('tbody')[0];
 zNode.innerHTML += '<tr><button id="set_profile" class="btn btn-secondary mt-2 mb-2 w-100" type="button">' + 'Set as my profile</button></tr>';
 document.getElementById("set_profile").addEventListener ("click", ButtonClickAction, false);
